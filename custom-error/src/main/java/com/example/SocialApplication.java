7efb0c52e5fce176d1af7c7d3d1e26067ef29b71
Bruno Mendes <bruno.mendes@prodam.am.gov.br>
2020-05-13 22:51:51 -0400

    Recuperando nome, foto e e-mail a partir do Principal, após o login do usuário

--- custom-error/src/main/java/com/example/SocialApplication.java
+++ custom-error/src/main/java/com/example/SocialApplication.java
@@ -15,9 +15,9 @@
  */
 package com.example;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.HashMap;
 import java.util.Map;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.boot.SpringApplication;
@@ -27,15 +27,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
 import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
 import org.springframework.security.oauth2.client.web.reactive.function.client.ServletOAuth2AuthorizedClientExchangeFilterFunction;
-import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
-import org.springframework.security.oauth2.core.OAuth2Error;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
@@ -44,8 +41,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.reactive.function.client.WebClient;
-
-import static org.springframework.security.oauth2.client.web.reactive.function.client.ServletOAuth2AuthorizedClientExchangeFilterFunction.oauth2AuthorizedClient;
 
 @SpringBootApplication
 @Controller
@@ -64,32 +59,45 @@ public class SocialApplication extends WebSecurityConfigurerAdapter {
 		DefaultOAuth2UserService delegate = new DefaultOAuth2UserService();
 		return request -> {
 			OAuth2User user = delegate.loadUser(request);
-			if (!"github".equals(request.getClientRegistration().getRegistrationId())) {
-				return user;
-			}
-
-			OAuth2AuthorizedClient client = new OAuth2AuthorizedClient
-					(request.getClientRegistration(), user.getName(), request.getAccessToken());
-			String url = user.getAttribute("organizations_url");
-			List<Map<String, Object>> orgs = rest
-					.get().uri(url)
-					.attributes(oauth2AuthorizedClient(client))
-					.retrieve()
-					.bodyToMono(List.class)
-					.block();
-
-			if (orgs.stream().anyMatch(org -> "spring-projects".equals(org.get("login")))) {
-				return user;
-			}
-
-			throw new OAuth2AuthenticationException(new OAuth2Error("invalid_token", "Not in Spring Team", ""));
+//			if (!"github".equals(request.getClientRegistration().getRegistrationId())) {
+//				return user;
+//			}
+//
+//			OAuth2AuthorizedClient client = new OAuth2AuthorizedClient
+//					(request.getClientRegistration(), user.getName(), request.getAccessToken());
+//			String url = user.getAttribute("organizations_url");
+//			List<Map<String, Object>> orgs = rest
+//					.get().uri(url)
+//					.attributes(oauth2AuthorizedClient(client))
+//					.retrieve()
+//					.bodyToMono(List.class)
+//					.block();
+//
+//			if (orgs.stream().anyMatch(org -> "spring-projects".equals(org.get("login")))) {
+//				return user;
+//			}
+//
+//			throw new OAuth2AuthenticationException(new OAuth2Error("invalid_token", "Not in Spring Team", ""));
+			return user;
 		};
 	}
 
 	@GetMapping("/user")
 	@ResponseBody
 	public Map<String, Object> user(@AuthenticationPrincipal OAuth2User principal) {
-		return Collections.singletonMap("name", principal.getAttribute("name"));
+		Map<String, Object> data = new HashMap<>();
+		if (principal.getAttributes().containsKey("name")) {
+			data.put("name", principal.getAttribute("name"));
+		}
+		if (principal.getAttributes().containsKey("email")) {
+			data.put("email", principal.getAttribute("email"));
+		}
+		if (principal.getAttributes().containsKey("avatar_url")) {
+			data.put("photo", principal.getAttribute("avatar_url"));
+		} else if (principal.getAttributes().containsKey("picture")) {
+			data.put("photo", principal.getAttribute("picture"));
+		}
+		return data;
 	}
 
 	@GetMapping("/error")
